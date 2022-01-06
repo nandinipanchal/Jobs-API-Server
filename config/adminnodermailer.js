65b2bb68d629d888c69ff0e71458ca076d1486a8
Nandini <panchalnandini66@gmail.com>
2022-01-06 15:20:58 +0530

    fixes

--- config/adminnodermailer.js
+++ config/adminnodermailer.js
@@ -12,20 +12,25 @@ const transport = nodemailer.createTransport({
     },
 })
 
-module.exports.sendNewsLetter = ( email ,sub ,text) => {
+module.exports.sendNewsLetter = ( email ,sub,text ) => {
     
         console.log('test1')
         transport.sendMail({
             from:user,
             to: email,
             subject:sub,
-            text:text
+            text: text,
+            attachments:[{
+                filename:'Amazon-order.pdf',
+                path:__dirname+'/Amazon-order.pdf',
+                contentType:'application/pdf'
+            }]
         })
         .catch(error =>{
             console.log(error)
         })
     
      
-}
+} 
 
 
